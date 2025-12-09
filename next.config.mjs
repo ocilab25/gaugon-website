3a55fb59ef46081fd815db75dcab1ae03eeee85b
ocilab25 <oci.lab25@proton.me>
2025-12-08 21:02:13 -0500

    Updated v1
    Updated next.config.mjs
    Updated .github/workflows/static.yml

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // Deploying to GitHub Pages subdirectory
+  basePath: '/gaugon-website',
+  trailingSlash: true,
+};
 
 export default nextConfig;
 
